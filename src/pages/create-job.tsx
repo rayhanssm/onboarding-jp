148722c699aec2f9534183264d6a559bd3d6e667
Ray Hans Sebastian Mantiri <rayhansmantiri@gmail.com>
2023-02-27 17:56:38 +0700

    add validation

--- src/pages/create-job.tsx
+++ src/pages/create-job.tsx
@@ -1,98 +1,170 @@
-import React from 'react'
-import { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button'
-import Card from '@mui/material/Card'
-// import { DatePicker } from '@mui/lab'
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { Box, FormControlLabel, Grid, TextField, Typography, FormControl, FormLabel, FormGroup, FormHelperText, Checkbox } from '@mui/material'
-import { useFormik } from 'formik'
-import * as yup from 'yup'
-import Link from 'next/link';
-
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import {
+  Box,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormHelperText,
+  Checkbox,
+  Stack,
+} from "@mui/material";
+import { Dayjs } from "dayjs";
+import Link from "next/link";
 
 function CreateJob() {
-    const formik = useFormik({
-        initialValues: {
-            title: '',
-            description: '',
-            openDate: '',
-            closeDate: '',
-        }, 
-        validationSchema: yup.object().shape({
-            title: yup.string().required("Title is required"),
-            description: yup.string().required("Description is required"),
-            
-        }),
-        onSubmit: () => {
-    
-        },
-      });
+  const methods = useForm({
+    resolver: yupResolver(
+      yup.object().shape({
+        title: yup
+          .string()
+          .required("Title is required")
+          .typeError("Title is required"),
+        description: yup
+          .string()
+          .required("Description is required")
+          .typeError("Title is required"),
+        openDate: yup
+          .date()
+          .required("Open date is required")
+          .typeError("Open date is required"),
+        closeDate: yup
+          .date()
+          .required("Close date is required")
+          .typeError("Close date is required"),
+      })
+    ),
+  });
 
-    const [date, setDate] = useState(null);
+  const { control, handleSubmit, register } = methods;
 
-    const handleChange = () => {
-        setDate(date);
-    }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
-    <Box sx={{flexGrow: 1}}>
-        <AppBar position='static'>
-            <Toolbar>
+      <Box sx={{ flexGrow: 1 }} marginBottom="24px">
+        <AppBar position="static">
+          <Toolbar>
             <Box sx={{ flexGrow: 1 }}>
-                <Button color="inherit" component={Link} href='/job-list'>Jobs</Button>
-                <Button color="inherit" component={Link} href='/hhh'>Applications</Button>
+              <Button color="inherit" component={Link} href="/job-list">
+                Jobs
+              </Button>
+              <Button color="inherit" component={Link} href="/hhh">
+                Applications
+              </Button>
             </Box>
-            <Button variant="contained" color="error" component={Link} href='/'>Logout</Button>
-            </Toolbar>
+            <Button variant="contained" color="error" component={Link} href="/">
+              Logout
+            </Button>
+          </Toolbar>
         </AppBar>
-    </Box>
-        <Card sx={{minWidth: '800px', padding:'24px'}}>
-            <FormGroup>
-                <Typography variant='h4' fontWeight='bold' marginBottom='32px' align='center'>Add Job</Typography>
-                <TextField 
-                    id="title"
-                    label="Title"
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.title && Boolean(formik.errors.title)}
-                    helperText={formik.touched.title && formik.errors.title}
-                    sx={{marginBottom: '16px'}} />
-                <TextField 
-                    id="description"
-                    label="Description" 
-                    multiline
-                    rows={4}
-                    maxRows={10}
-                    value={formik.values.description} 
-                    onChange={formik.handleChange} 
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.description && Boolean(formik.errors.description)}
-                    helperText={formik.touched.description && formik.errors.description}
-                    sx={{marginBottom: '16px'}} />
-                    <DatePicker
-                        label="Open Date"
-                        inputFormat="DD/MM/YYYY"
-                        value={date}
-                        onChange={handleChange}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                    <DatePicker
-                        label="Close Date"
-                        inputFormat="DD/MM/YYYY"
-                        value={date}
-                        onChange={handleChange}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                <Button variant='contained' size='large' type='submit' color='primary'>Create</Button>
-            </FormGroup>
-        </Card>
+      </Box>
+      <Card sx={{ minWidth: "800px", padding: "24px" }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            marginBottom="32px"
+            align="center"
+          >
+            Add Job
+          </Typography>
+          <Stack spacing={2}>
+            <Controller
+              name="title"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  label="Title"
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+            <Controller
+              name="description"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  label="Description"
+                  multiline
+                  rows={4}
+                  maxRows={10}
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Controller
+                name="openDate"
+                control={control}
+                render={({ field, fieldState: { error } }) => (
+                  <DatePicker
+                    {...field}
+                    label="Open Date"
+                    inputFormat="DD/MM/YYYY"
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        error={!!error}
+                        helperText={error?.message}
+                      />
+                    )}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Controller
+                name="closeDate"
+                control={control}
+                render={({ field, fieldState: { error } }) => (
+                  <DatePicker
+                    {...field}
+                    label="Close Date"
+                    inputFormat="DD/MM/YYYY"
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        error={!!error}
+                        helperText={error?.message}
+                      />
+                    )}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+            <Button
+              variant="contained"
+              size="large"
+              type="submit"
+              color="primary"
+            >
+              Create Job
+            </Button>
+          </Stack>
+        </form>
+      </Card>
     </>
-  )
+  );
 }
 
-export default CreateJob
+export default CreateJob;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,61 +1,105 @@
-import React from 'react'
-import Button from '@mui/material/Button'
-import Card from '@mui/material/Card'
-import { Box, FormControlLabel, Grid, TextField, Typography, FormControl, FormLabel, FormGroup, FormHelperText, Checkbox } from '@mui/material'
-import { useFormik } from 'formik'
-import * as yup from 'yup'
-import Link from 'next/link'
+import React from "react";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import {
+  Box,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormHelperText,
+  Checkbox,
+  Stack,
+} from "@mui/material";
+import Link from "next/link";
 
 function Login() {
-    const formik = useFormik({
-      initialValues: {
-          email: '',
-          password: '',
-      }, 
-      validationSchema: yup.object().shape({
-          email: yup.string().email("Enter unique email").required('Required'),
-          password: yup.string().required("Password is required"),
-      }),
-      onSubmit: () => {
-  
-      },
-    });
+  const methods = useForm({
+    resolver: yupResolver(
+      yup.object().shape({
+        email: yup
+          .string()
+          .email("Enter unique email")
+          .required("Email is required")
+          .typeError("Email is required"),
+        password: yup
+          .string()
+          .required("Password is required")
+          .typeError("Password is required"),
+      })
+    ),
+  });
+
+  const { control, handleSubmit, register } = methods;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <Grid 
-        container
-        spacing={0}
-        direction= 'column'
-        alignItems= 'center'
-        justifyContent= 'center'
-        sx={{minHeight: '100vh'}}>
-        <Card sx={{minWidth: '800px', padding:'24px'}}>
-            <FormGroup>
-                <Typography variant='h4' fontWeight='bold' marginBottom='32px' align='center'>Login</Typography>
-                <TextField 
-                    id="email"
-                    label="Email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                    sx={{marginBottom: '16px'}} />
-                <TextField 
-                    id="password"
-                    label="Password"
-                    type="password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
-                    sx={{marginBottom: '16px'}} />
-                <Button variant='contained' size='large' type='submit' color='primary'>Login</Button>
-            </FormGroup>
-        </Card>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh" }}
+    >
+      <Card sx={{ minWidth: "800px", padding: "24px" }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            marginBottom="32px"
+            align="center"
+          >
+            Login
+          </Typography>
+          <Stack spacing={2}>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  label="Email"
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  label="Password"
+                  type="password"
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+            <Button
+              variant="contained"
+              size="large"
+              type="submit"
+              color="primary"
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </Card>
     </Grid>
-  )
+  );
 }
 
-export default Login
+export default Login;
